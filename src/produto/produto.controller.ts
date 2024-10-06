9fbadf1ffed6ebc9aebc5d32ac4572a734599068
Kayk-Rios <manjac688@gmail.com>
2024-10-06 14:18:48 -0300

    feat:CriaçaoDeMetodosGetProdutos_ProdutoId

--- src/produto/produto.controller.ts
+++ src/produto/produto.controller.ts
@@ -1,20 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-import { produtos, Produto } from 'src/core';
+import { Controller, Get, Param } from "@nestjs/common";
+import { produtos, Produto } from "src/core";
 
-@Controller('produtos')
+@Controller("produtos")
 export class ProdutoController {
-        
-    @Get()
-    obterProdutos() : Produto []{
-        return produtos.map((produto)=>({
-
-                ...produto,
-                especificacoes: {destaque: produto.especificacoes.destaque},
-
-        }))
-
-    }
-
-
-
+  @Get()
+  async obterProdutos(): Promise<Produto[]> {
+    return produtos.map((produto) => ({
+      ...produto,
+      especificacoes: { destaque: produto.especificacoes.destaque },
+    }));
+  }
+  @Get(":id")
+  async obterProdutoPorId(@Param("id") id: string): Promise<Produto | null> {
+    const produto = produtos.find((produto) => produto.id === +id);
+    return produto ?? null;
+  }
 }
